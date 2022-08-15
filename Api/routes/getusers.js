5dbd4b15aabe1a56979cf937fabf3add39b6a7ad
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-08-15 14:39:33 -0300

    routes

---
+++ Api/routes/getusers.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "prueba",
+});
+
+router.get("/", (req, res) => {
+  const sql = "SELECT * FROM usuarios";
+
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send("Not result");
+    }
+  });
+});
+
+module.exports = router;
