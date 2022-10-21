563d7c97112d7c2f0918ee0ece101035c43bb62b
RodrigoFdz01 <rodrigof3012@gmail.com>
2022-10-21 18:36:13 -0300

    get with js

---
+++ web/getAllusers.js
@@ -0,0 +1,17 @@
+let getAll = document.getElementById("allUsers");
+
+getAll.addEventListener("click", function () {
+  fetch("http://localhost:3000/users")
+    .then((response) => response.json())
+    .then((data) => {
+      let names = data;
+      //console.log(names);
+      names.map(function (e) {
+        let lu = document.createElement("ul");
+        lu.innerHTML = `<h3>${e.name}</h3>`;
+        let divContainer = document.getElementById("div2");
+        divContainer.appendChild(lu);
+        //console.log(e);
+      });
+    });
+});
